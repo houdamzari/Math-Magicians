30b7e6d16bb3024b021d1115a1e682c1e8f2628a
houdamzari <houdamzari1999@gmail.com>
2023-01-10 12:14:01 +0100

    Add calculator grid

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noreferrer">
-          Learn React
-        </a>
-      </header>
+      <Calculator />
     </div>
   );
 }
